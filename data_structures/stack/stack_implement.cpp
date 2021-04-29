1f97bb643acdbf32fb87c86c0a1f81daab6d055b
P V Santosh <pvsantosh2019@gmail.com>
2021-04-29 09:54:50 +0530

    update stack_implement.cpp

--- data_structures/stack/stack_implement.cpp
+++ data_structures/stack/stack_implement.cpp
@@ -1,1 +1,20 @@
-// This will contain stack implementation using arrays
+// This will contain stack implementation using STL
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+  stack<int> s;
+  s.push(4);// pushes an element into the stack
+  s.push(2);
+  s.push(3);
+  s.push(8);
+  
+  cout<<s.top()<<"\n"; // prints the top element at the stack
+  
+  while(!s.empty()){ // checks if the stack is empty or not
+    cout<< s.top<<" ";
+    s.pop(); // removes the element from the stack
+  }
+  
+  return 0;
+}
