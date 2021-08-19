44a895cc9cff5a070dc36a88c70fae929610d330
P V Santosh <pvsantosh2019@gmail.com>
2021-08-19 20:02:03 +0530

    create dfs.cpp

---
+++ algorithms/dfs.cpp
@@ -0,0 +1,9 @@
+void Graph::DFS(int v)
+{   
+    visited[v] = true;
+    cout << v << " ";
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i)
+        if (!visited[*i])
+            DFS(*i);
+}
