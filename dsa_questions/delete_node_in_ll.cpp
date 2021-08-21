585fae5f7ac800bf4715b7e63578a9d108ae6512
P V Santosh <pvsantosh2019@gmail.com>
2021-08-21 12:17:36 +0530

    Create delete_node_in_ll.cpp

---
+++ dsa_questions/delete_node_in_ll.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// question link -> https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
